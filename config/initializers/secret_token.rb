7ca74f1be6e012f2a860c546a25e9f27f8c14031
David Frey <watchtower@facesofdave.com>
2012-08-16 21:27:04 -0700

    Create new Rails 3.2.8 App without Test::Unit
    Command issued: `rails new my party --skip-test-unit`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myparty::Application.config.secret_token = '4822485f345862a5d71e6a7d035f020b762b932f5fdb9033da43738b7b05ea68db78aa0657f3cbe8b4d335243d2192a7ff1e2b496295107bb9e4a2e45c972f4c'
